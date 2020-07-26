2a9d89000d975b4786f29901373025455798a5be
AMAN KUMAR <67097430+KrAman27@users.noreply.github.com>
2020-07-26 19:24:46 +0530

    Add files via upload
    Assignment 3 : To build a SVR model.

---
+++ model_Ass_3.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 18:23:34 2020
+
+@author: aman kumar
+"""
+
+
+#importing the libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importing dataset
+dataset = pd.read_csv('Car_Purchasing_Data (1).csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, [8]].values
+
+#training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+y_train = sc_y.fit_transform(y_train)
+X_test = sc_X.transform(X_test)
+
+#fitting the SVR to the dataset
+from sklearn.svm import SVR
+regressor = SVR(kernel='rbf')
+regressor.fit(X_train,y_train)
+
+#Predicting a new result
+y_Pred=sc_y.inverse_transform(regressor.predict(X_test))   
+
+#creating the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_Pred)
